55c39d824817b2a721946ef44f2833acfde3182f
Derek Charlton <dc455@byu.edu>
2023-02-06 22:45:44 -0700

    First push to mission 05

---
+++ Mission04_dc455/Controllers/BlahController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_dc455.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(object model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission04_dc455/Models/GradeCalculatorModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_dc455.Models
+{
+    public class GradeCalculatorModel
+    {
+        public string assignments{get; set;}
+    }
+}
