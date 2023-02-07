c5edba0e5dd04fd4e670e3920dfb30dbe257118a
Derek Charlton <dc455@byu.edu>
2023-02-07 13:39:35 -0700

    Added float input range as a number between 0 and 100

--- Mission04_dc455/Models/GradeCalculatorModel.cs
+++ Mission04_dc455/Models/GradeCalculatorModel.cs
@@ -8,8 +8,11 @@ namespace Mission04_dc455.Models
 {
     public class GradeCalculatorModel
     {
-
+        // This makes it so that none of the fields can be blank
         [Required]
+        //This makes it so that the input has to be a number between 0 and 100
+
+        [Range(0.0, 100.0)]
         public float assignments{get; set;}
         public float group { get; set; }
         public float quizzes { get; set; }
